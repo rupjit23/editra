fbd3cd4f78af25409a1fc4d68e669dcd054e2e7c
Rupjit Maity <121430392+rupjit23@users.noreply.github.com>
2025-09-08 16:56:34 +0530

    Update socket.jsx

--- Client/src/socket.jsx
+++ Client/src/socket.jsx
@@ -1,19 +1,18 @@
+
 import { io } from "socket.io-client";
 
-const BACKEND_URL = "https://editra.onrender.com"; 
+const BACKEND_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000" // local backend
+    : "https://editra.onrender.com"; // deployed backend
 
 export const initSocket = async () => {
   const options = {
-    'force new connection': true,
+    forceNew: true,
     reconnectionAttempts: Infinity,
     timeout: 10000,
-    transports: ['websocket'],
+    transports: ["websocket"],
   };
 
   return io(BACKEND_URL, options);
 };
-
-
-
-
-
